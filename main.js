07a492e14fbe11a1d33df9b0a5b69debbc17e028
leocabbage2008 <70451398+leocabbage2008@users.noreply.github.com>
2021-02-11 12:45:08 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,2 @@
+//paste this into console, not self xss :)
+document.getElementsByClassName('dwrYTb PK')[0].click();var l = document.getElementsByClassName('iCWmEc');var counter = 0; setInterval(function func(){l[counter].click(); document.getElementsByClassName('LbWtad cmX6We Wxsg2c hH1ADf z-b-ua')[0].click(); counter++}, 500)
