887d48acd10bfaf882215f45d2e3da06402739b0
leocabbage2008 <70451398+leocabbage2008@users.noreply.github.com>
2021-02-11 13:03:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 //paste this into console, not self xss :)
-document.getElementsByClassName('dwrYTb PK')[0].click();var l = document.getElementsByClassName('iCWmEc');var counter = 0; setInterval(function func(){l[counter].click(); document.getElementsByClassName('LbWtad cmX6We Wxsg2c hH1ADf z-b-ua')[0].click(); counter++}, 500)
+const del = setInterval(function func(){
+  document.getElementsByClassName('dwrYTb PK')[0].click();
+  var l = document.getElementsByClassName('iCWmEc');
+  try{
+    l[0].click();
+    document.getElementsByClassName('LbWtad cmX6We Wxsg2c hH1ADf z-b-ua')[0].click();
+  }
+  catch{
+    //pass
+  }
+}, 500)
+document.body.addEventListener("click", function f(){clearInterval(del)});
